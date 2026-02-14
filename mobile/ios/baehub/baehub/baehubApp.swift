702a5aa0d74313df3e3219d457a301dac0e3beac
Jezer Crespo <jezer@resolutionapp.co.nz>
2026-02-15 02:29:37 +0800

    Fix navigation bar title color on scroll

--- mobile/ios/baehub/baehub/baehubApp.swift
+++ mobile/ios/baehub/baehub/baehubApp.swift
@@ -5,10 +5,26 @@ import HotwireNative
 struct baehubApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
 
+    init() {
+        configureNavigationBarAppearance()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .ignoresSafeArea()
         }
+    }
+
+    private func configureNavigationBarAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .systemBackground
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.label]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.label]
+
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
 }
