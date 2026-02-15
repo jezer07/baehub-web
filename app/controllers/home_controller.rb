194252afcc98a3c5544bd71a1285502c705d1295
Jezer Crespo <jezer@resolutionapp.co.nz>
2026-02-15 11:18:38 +0800

    Add privacy policy page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
   def index
     redirect_to new_user_session_path if hotwire_native_app?
   end
+
+  def privacy_policy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,6 @@ Rails.application.routes.draw do
 
   unauthenticated do
     root "home#index"
+    get "privacy_policy", to: "home#privacy_policy"
   end
 end
