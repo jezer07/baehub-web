e35fba39b9b5f2e4feef14c091bdd2d65938b133
Jezer Crespo <jezer@resolutionapp.co.nz>
2026-02-15 12:01:36 +0800

    Address PR feedback: update privacy policy content, add footer link, and add test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -8,6 +8,12 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_match "Plan, love, and live in sync.", response.body
   end
 
+  test "should get privacy policy" do
+    get privacy_policy_path
+    assert_response :success
+    assert_select "h1", "Privacy Policy"
+  end
+
   test "native root redirects to sign in for unauthenticated users" do
     get root_path, headers: { "HTTP_USER_AGENT" => "Hotwire Native iOS" }
 
